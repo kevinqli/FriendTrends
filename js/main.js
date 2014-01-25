1e16fe1f75e4ee24d33a17f91a039a58d10c61da
Anurag Bakshi <anuragbakshi123@gmail.com>
2014-01-25 07:39:56 -0800

    Added message scanner

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
 function loadPredictionAPI(onComplete) {
 	gapi.client.setApiKey("AIzaSyAkCIbrL4raFaeRARBL2tzyTMdJEd2NcAA");
-	gapi.client.load('prediction', 'v1.6', onComplete);
+	gapi.client.load('prediction', 'v1.6');
+}
+
+function loadMore(itr, onComplete) {
+	if(itr === 0) onComplete();
+
+	moreBtn = $("a._5pc7")[0];
+	if(moreBtn == undefined) {
+		onComplete();
+		return;
+	}
+
+	moreBtn.click();
+
+	setTimeout(function() {
+		loadMore(--itr, onComplete);
+	}, 2000);
 }
 
 function main() {
+	loadMore(10, function() {
+		var messages = $(".mbs._5pbx.userContent");
+		var containers = messages.parents("._5jmm._5pat._5uch");
 
+		console.log(messages.length);
+	});
 }
 
-loadPredictionAPI(main);
+// loadPredictionAPI(main);
+$(function() {
+	main();
+});
